352d768b2b5dc2325a8a66dc9ef2410ac79269f8
Manikandan <itzmanii2005@gmail.com>
2026-06-29 13:01:05 +0530

    Solved: Deque Traversal - GFG

---
+++ Deque/c_deque_traversal.java
@@ -0,0 +1,14 @@
+// User solved: Deque Traversal
+// Problem URL: https://www.geeksforgeeks.org/batch/dsa-self-paced-original-2026/track/DSASPDeque/problem/dequeue-traversal
+// Language: Java
+// Synced using GFG -> GitHub Sync Chrome Extension
+
+class Solution {
+    public static void printDeque(ArrayDeque<Integer> deq) {
+        // code here
+        for(int x:deq){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+    }
+}
