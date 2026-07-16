35f425479da76e8900d890be37b20763dd74ca08
Manikandan <itzmanii2005@gmail.com>
2026-07-17 00:58:51 +0530

    Solved: Odd Divisor Count - GFG

---
+++ Mathematics/Contest_odd_divisor_count.java
@@ -0,0 +1,11 @@
+// User solved: Odd Divisor Count
+// Problem URL: https://practice.geeksforgeeks.org/contest/mathematics-1614/problems
+// Language: Java
+// Synced using GFG -> GitHub Sync Chrome Extension
+
+class Solution {
+    public int oddDivisorsCount(int n) {
+        // code here
+        return (int)Math.sqrt(n)-1;
+    }
+}
