5fe6761d618a34ef0fc186b7961c55c5e12ed5fd
Manikandan <itzmanii2005@gmail.com>
2026-07-22 23:45:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse Array Using Stack/reverse-array-using-stack.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseArray(int[] arr) {
+        // code here
+        Stack<Integer> st=new Stack<>();
+        for(int i:arr){ 
+            st.push(i);
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=st.pop();
+        }
+    }
+}
