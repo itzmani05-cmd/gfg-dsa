5465ecf30e99e6f386e43ebaeb6805a20f3ec27d
Manikandan <itzmanii2005@gmail.com>
2026-07-23 00:04:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array to Stack/array-to-stack.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    // Push elements of an array into a stack.
+    public static Stack<Integer> push(int arr[]) {
+        
+        // code here
+        Stack<Integer> st=new Stack<>();
+        for(int i:arr){
+            st.push(i);
+            
+        }
+        return st;
+        
+    }
+
+    // Print elements of a stack and pop them.
+    public static void printAndPop(Stack<Integer> st) {
+        // code here
+        while(!st.isEmpty()){ 
+            System.out.print(st.pop() +" "); 
+        }
+    }
+}
